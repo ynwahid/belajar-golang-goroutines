61147e32fc129b82d2eff49e663ddbda517ee88a
ynwahid <yusufedu99@gmail.com>
2025-08-24 20:37:08 +0700

    cond

---
+++ cond_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+// TODO: Relearn sync.Cond later
+
+var (
+	locker = &sync.Mutex{}
+	cond   = sync.NewCond(locker)
+	wg     = sync.WaitGroup{}
+)
+
+func WaitCondition(value int) {
+	defer wg.Done()
+	wg.Add(1)
+
+	cond.L.Lock()
+	cond.Wait()
+	fmt.Println("Done", value)
+	cond.L.Unlock()
+}
+
+func TestCond(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		go WaitCondition(i)
+	}
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			time.Sleep(1 * time.Second)
+			cond.Signal()
+		}
+	}()
+
+	// go func() {
+	// 	time.Sleep(1 * time.Second)
+	// 	cond.Broadcast()
+	// }()
+	wg.Wait()
+}
